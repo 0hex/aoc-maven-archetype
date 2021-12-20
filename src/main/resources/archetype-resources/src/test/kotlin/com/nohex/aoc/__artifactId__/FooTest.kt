6a1229549324616e2c9c7e5f157089ea45eec268
Max Noé <code@0h.io>
2021-12-20 09:42:26 +0100

    Remove JUnit and AssertJ dependencies. Use only Kotest.

--- src/main/resources/archetype-resources/src/test/kotlin/com/nohex/aoc/__artifactId__/FooTest.kt
+++ src/main/resources/archetype-resources/src/test/kotlin/com/nohex/aoc/__artifactId__/FooTest.kt
@@ -1,13 +1,12 @@
 package com.nohex.aoc.${artifactId}
 
+import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Test
 
-internal class FooTest {
-    @Test
-    fun testExample() {
+internal class FooTest: StringSpec({
+    "The example puzzle input produces the reported result" {
         val input = getInput("example.txt")
         val sut = Foo(input)
         sut.getSolution() shouldBe 0
     }
-}
+})
