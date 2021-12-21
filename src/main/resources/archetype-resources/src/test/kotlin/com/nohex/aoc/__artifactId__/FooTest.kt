b3b89918367e60d78eee1e67c3926e8b507abc56
Max Noé <code@0h.io>
2021-12-21 19:11:32 +0100

    Use the ShouldSpec style by default.

--- src/main/resources/archetype-resources/src/test/kotlin/com/nohex/aoc/__artifactId__/FooTest.kt
+++ src/main/resources/archetype-resources/src/test/kotlin/com/nohex/aoc/__artifactId__/FooTest.kt
@@ -1,12 +1,15 @@
 package com.nohex.aoc.${artifactId}
 
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 
-internal class FooTest: StringSpec({
-    "The example puzzle input produces the reported result" {
+internal class FooTest : ShouldSpec({
+    context("The example puzzle input") {
         val input = getInput("example.txt")
         val sut = Foo(input)
-        sut.getSolution() shouldBe 0
+
+        should("produce the expected result") {
+            sut.getSolution() shouldBe 0
+        }
     }
 })
